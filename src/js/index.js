a522dcce0d1b5b3174e89e02d8037a896e7f9eab
Kesha <kesha.leushin@gmail.com>
2018-12-04 18:51:39 +0300

    first step

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,25 @@
 import "../css/style.css";
+
+let myMap, clusterer;
+
+ymaps.ready(async () => {
+    try {
+        myMap = new ymaps.Map('map', {
+            center: [59.95, 30.3],
+            zoom: 11,
+            controls: ['zoomControl', 'searchControl']
+        }, {
+            searchControlProvider: 'yandex#search'
+        });
+
+        clusterer = new ymaps.Clusterer({
+            preset: 'islands#invertedBlackClusterIcons',
+            clusterDisableClickZoom: true,
+            openBalloonOnClick: true
+        });
+
+        myMap.geoObjects.add(clusterer);
+    } catch(e) {
+        console.error(e.message);
+    }
+});
